4c16e4c3ee2a0029afab06d7c243d21547c00cb1
YimKeul <leesjun29@gmail.com>
2023-03-16 12:19:44 +0900

    feat : carDetail View

--- StudyListNavDemo/ContentView.swift
+++ StudyListNavDemo/ContentView.swift
@@ -8,19 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var carStore : CarStore = CarStore(cars: carData)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List{
+            ForEach(carStore.cars){ car in
+                ListCell(car : car)
+                
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ListCell: View {
+    var car : Car
+    var body: some View {
+        HStack{
+            Image(car.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 100 , height: 60)
+            Text(car.name)
+        }
     }
 }
