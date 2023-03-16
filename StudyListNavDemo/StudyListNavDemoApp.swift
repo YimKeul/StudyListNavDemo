5ee5b6827379c49bcca268d1f3e7e8e85d691069
YimKeul <leesjun29@gmail.com>
2023-03-16 11:59:35 +0900

    Initial Commit

---
+++ StudyListNavDemo/StudyListNavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyListNavDemoApp.swift
+//  StudyListNavDemo
+//
+//  Created by yimkeul on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct StudyListNavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
